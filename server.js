c4af649f9a1b6f687bfb0dc1a9583a253edcddf5
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-17 10:28:40 +0200

    modify the port

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/add", (req, res) => {
 
 
 // Setup Server
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 app.listen(port, () => {
     console.log(`listening on port: ${ port }`);
 })
